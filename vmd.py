2b1a0093e4d58f5dac361a94e9f86b41b0769829
Oleg-Balakin <balakin-o-v@yandex.ru>
2023-04-08 17:52:52 +0300

    First commit

---
+++ vmd.py
@@ -0,0 +1,24 @@
+import cv2
+
+cap = cv2.VideoCapture(0)   # 0 – первая веб-камера в системе, 1 – вторая и т.д.
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    ret, new_fraim = cap.read()
+
+    diff = cv2.absdiff(frame, new_fraim)              # переменная с разницей между пикселями кадров
+    gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)      # преобразование разницы в серый цвет
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)          # сглаживание по Гауссу, высота и ширина 5, без отклонений
+    _, threshold = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)   # пороговое значение для фиксации изменений
+    dilate = cv2.dilate(threshold, None, iterations=3)  # фильтр для расширения изображения
+    contours, _ = cv2.findContours(dilate, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)  # поиск контуров изображения
+
+    for c in contours:
+        if cv2.contourArea(c) < 5000:                     # вычисление площади контура
+            continue
+        x, y, w, h = cv2.boundingRect(c)               # определение правой верхней координаты, ширины и высоты контура
+        cv2.rectangle(frame, (x, y), (x + w, y + y), (0, 255, 0), 2)  # метод рисует прямоугольник
+
+    if cv2.waitKey(10) == ord('q'):                    # открытое окно пока не нажата кнопка q
+        break
+    cv2.imshow('Camera', frame)
